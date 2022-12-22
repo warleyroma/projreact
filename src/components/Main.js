166df39c5c071dcfe6ba63b21901433c6d3de8c8
warley <warleyroma@gmail.com>
2022-12-22 19:53:56 -0300

    Estilizando as tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -25,7 +25,7 @@ export default class Main extends Component {
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
-        <form action="#">
+        <form action="#" className="form">
           <input onChange={this.handleChange} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
